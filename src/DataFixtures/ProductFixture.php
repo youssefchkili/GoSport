b854fe9c48497efe1ee6464c6ba97cbe1420ca45
youssef <youssefchkili04@gmail.com>
2025-05-10 21:34:04 +0200

    Merge branch 'main' of https://github.com/youssefchkili/GoSport fixing git repository#

---
+++ src/DataFixtures/ProductFixture.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\ProductImage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProductFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = \Faker\Factory::create();
+        $product = new Product();
+        $product->setName($faker->word);
+        $product->setSlug($faker->name);
+        $product->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        $product->setPrice(50);
+        $product->setDiscountPercent(0.5);
+        $product->setStockQuantity($faker->numberBetween(1, 100));
+        
+        $category = new Category();
+        $category->setName($faker->word);
+        $category->setSlug($faker->name);
+        $category->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        $manager->persist($category);
+
+        $product->setCategoryId($category);
+        $manager->persist($product);
+
+        $productImage = new ProductImage();
+        $productImage->setProductId($product);
+        $productImage->setImagePath("https://example.com/image.jpg");
+        $productImage->setIsPrimary(true);
+
+        $manager->persist($productImage);
+
+        $manager->flush();
+    }
+}
