f4f3112df99c8c3ea64e6910764e0b5d1e8db3fd
yasserchouket <yasserchouket0102@gmail.com>
2025-05-12 11:41:23 +0100

    add profile security registration and home

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\ProfileForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+
+#[IsGranted('ROLE_USER')]
+final class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        // Get user's orders
+        $orders = $entityManager->getRepository('App\Entity\Order')
+            ->findBy(['user_id' => $user->getUserIdentifier()], ['created_at' => 'DESC']);
+
+        // Get user's address
+        $address = $user->getAdress();
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+            'orders' => $orders,
+            'address' => $address
+        ]);
+
+    }
+
+    #[Route('/profile/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileForm::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your profile has been updated!');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user
+        ]);
+
+    }
+    #[Route('/profile/change-password', name: 'app_profile_change_password')]
+    public function changePassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('newPassword')->getData()
+                )
+            );
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your password has been changed!');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/change_password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
